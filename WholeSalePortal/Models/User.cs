2591bc0e09fb9e97b15ac8ba9631d42f6a09e527
Matt Nygren <MattNygren_7@hotmail.com>
2019-01-02 00:25:10 -0800

    vscode additions

--- WholeSalePortal/Models/User.cs
+++ WholeSalePortal/Models/User.cs
@@ -11,5 +11,11 @@ namespace WholeSalePortal.Models
         public string Username { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+
+        public DateTime Created {get;set;}
+        public string AboutBroker {get;set;}
+        public string City {get;set;}
+
+        public ICollection<Photo> Photos {get;set;}
     }
 }
